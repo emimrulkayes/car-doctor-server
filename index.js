aee4d9bf8878b2efaf0f011a115d2be5dd6a67a6
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-05-05 13:09:44 +0600

    handle create post and delete

--- index.js
+++ index.js
@@ -34,6 +34,21 @@ async function run(){
             const service = await serviceCollection.findOne(query);
             res.send(service);
         })
+
+        // POST
+        app.post('/service', async(req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        // DELETE
+        app.delete('/service/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally{
 
@@ -52,4 +67,6 @@ app.get('/', async(req, res) => {
 app.listen(port, () => {
     console.log(`car doctor server is running, ${port}`)
 });
+
+
 
